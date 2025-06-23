cacbdca24b438904887f96048f3340ad9d8b6d1f
WilsonNous <107373913+WilsonNous@users.noreply.github.com>
2025-06-23 15:24:10 -0300

    Create config.py

---
+++ config.py
@@ -0,0 +1,8 @@
+# config.py
+
+DB_CONFIG = {
+    "host": "localhost",  # Ou o host fornecido pelo HostGator
+    "user": "noust785_edi_admin",
+    "password": "N3tunn@21#",
+    "database": "noust785_edi_ops"
+}
