7ee6f91bbeeb243c1a7c4583aea2351989592920
WilsonNous <107373913+WilsonNous@users.noreply.github.com>
2025-09-18 17:18:17 -0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 # Script de build para o Render
 
-echo "Instalando dependências..."
+echo "Instalando dependências Python..."
 pip install -r requirements.txt
+
+echo "Configurando permissões..."
+chmod a+x app.py
 
 echo "Build completo!"
