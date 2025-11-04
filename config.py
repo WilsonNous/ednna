2e08a704ab0174e3ae3fef0204fae08885611fc2
WilsonNous <107373913+WilsonNous@users.noreply.github.com>
2025-11-04 17:32:37 -0300

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,12 @@
 # config.py
+import os
 
 DB_CONFIG = {
-    "host": "localhost",  # Ou o host fornecido pelo HostGator
-    "user": "noust785_edi_admin",
-    "password": "N3tunn@21#",
-    "database": "noust785_edi_ops"
+    "host": os.getenv("DB_HOST", "localhost"),  # Default local
+    "user": os.getenv("DB_USER", "noust785_edi_admin"),
+    "password": os.getenv("DB_PASSWORD", "N3tunn@21#"),
+    "database": os.getenv("DB_NAME", "noust785_edi_ops"),
+    "port": int(os.getenv("DB_PORT", 3306)),
+    "charset": "utf8mb4",
+    "collation": "utf8mb4_unicode_ci"
 }
